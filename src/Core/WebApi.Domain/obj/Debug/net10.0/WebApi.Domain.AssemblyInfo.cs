2d9a74fea1a266c64cdf7e4d6c5ceda293d02980
agasoyelnardev <peyman@Peymans-MacBook-Air.local>
2026-07-19 16:27:50 +0400

    user-secrets

--- src/Core/WebApi.Domain/obj/Debug/net10.0/WebApi.Domain.AssemblyInfo.cs
+++ src/Core/WebApi.Domain/obj/Debug/net10.0/WebApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fdbda6c767c9474b21b29ad2f8e97a194c749c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8bc74a858223ad2892cab6b72b766dfcf3a894")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
